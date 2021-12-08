f1482420c5ed26fb3cbf1411e51ebab08139a93e
Soyeong Choi <simc2644@gmail.com>
2021-12-08 14:50:19 +0900

    Site updated: 2021-12-08 14:50:14

---
+++ images/test.js
@@ -0,0 +1,79 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+const fullScreen = player.querySelector(".fullScreen");
+
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgess() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.addEventListener("onload", (event) => {
+    player.currentTime = scrubTime;
+  });
+}
+
+function makeFullScreen() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) {
+    /* Firefox */
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    /* IE/Edge */
+    video.msRequestFullscreen();
+  }
+}
+
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", handleProgess);
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
+fullScreen.addEventListener("click", makeFullScreen);
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 32) {
+    // spacebar
+    e.preventDefault();
+    togglePlay();
+  }
+});
