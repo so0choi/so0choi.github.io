cc8df2a4a10fd083ee0b66a6f40d9b3207178777
sychoi <simc2644@gmail.com>
2025-11-08 19:07:57 +0900

    add search bar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://so0choi.github.io',
-    integrations: [mdx(), sitemap(), partytown(),],
+  site: "https://so0choi.github.io",
+  integrations: [mdx(), sitemap(), partytown(), pagefind()],
 });
