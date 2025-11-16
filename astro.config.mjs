1e019bf0847dfae1e94dcb1fb5eb43fa02605e58
sychoi <simc2644@gmail.com>
2025-11-17 02:41:46 +0900

    fix styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,27 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import partytown from "@astrojs/partytown";
 import pagefind from "astro-pagefind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://so0choi.github.io",
   integrations: [mdx(), sitemap(), partytown(), pagefind()],
+  experimental: {
+    fonts: [
+      {
+        name: "Playfair Display",
+        cssVariable: "--font-playfair-display",
+        provider: fontProviders.google(),
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
