55158a9a9efae6f978cf9d7d8c21d4b667393604
Soyeong Choi <simc2644@gmail.com>
2025-11-25 01:57:25 +0900

    fix styles and posting

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,12 @@ import pagefind from "astro-pagefind";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://so0choi.github.io",
-  integrations: [sitemap(), partytown(), pagefind()],
+  integrations: [sitemap(), partytown(), pagefind(), mdx()],
   experimental: {
     fonts: [
       {
